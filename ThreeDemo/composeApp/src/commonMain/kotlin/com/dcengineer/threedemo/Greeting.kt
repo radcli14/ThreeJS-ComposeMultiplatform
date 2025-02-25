84c8a6879184c913f8a462777718161a2b6d4efb
radcli14 <radcli14@gmail.com>
2025-02-25 15:37:32 +0100

    deleted unnnecessary code and resources

--- ThreeDemo/composeApp/src/commonMain/kotlin/com/dcengineer/threedemo/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.dcengineer.threedemo
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- ThreeDemo/composeApp/src/commonMain/kotlin/com/dcengineer/threedemo/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.dcengineer.threedemo
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
