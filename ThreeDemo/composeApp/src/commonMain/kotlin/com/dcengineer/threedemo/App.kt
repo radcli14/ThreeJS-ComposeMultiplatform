7cedbff40f293699d53801fb58c5e4ba2fed76f9
radcli14 <radcli14@gmail.com>
2025-02-25 19:10:12 +0100

    working up through cube animation

--- ThreeDemo/composeApp/src/commonMain/kotlin/com/dcengineer/threedemo/App.kt
+++ ThreeDemo/composeApp/src/commonMain/kotlin/com/dcengineer/threedemo/App.kt
@@ -1,27 +1,40 @@
 package com.dcengineer.threedemo
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewStateWithHTMLData
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import threedemo.composeapp.generated.resources.Res
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
-    val html = """
-    <html>
-        <body>
-            <h1>Three.js will go here!</h1>
-            <p>Soon with JavaScript...</p>
-        </body>
-    </html>
-    """.trimIndent()
+    var html by remember { mutableStateOf("") }
+    var js by remember { mutableStateOf("") }
+    LaunchedEffect(Unit) {
+        html = Res.readBytes("files/index.html").decodeToString()
+        js = Res.readBytes("files/cube.js").decodeToString()
+    }
+
+    MaterialTheme {
+        ThreeJsWebView(html.replace("/*CODE*/", js))
+    }
+}
+
+@Composable
+fun ThreeJsWebView(html: String) {
     val webViewState = rememberWebViewStateWithHTMLData(
         data = html
     )
-
-    MaterialTheme {
-        WebView(webViewState)
-    }
+    WebView(webViewState, modifier = Modifier.fillMaxSize())
 }
