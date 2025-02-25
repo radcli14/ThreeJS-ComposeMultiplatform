5375793421c0f1599b8e58bc47bbdb583d208593
radcli14 <radcli14@gmail.com>
2025-02-25 17:19:43 +0100

    load webview from HTML

--- ThreeDemo/composeApp/src/commonMain/kotlin/com/dcengineer/threedemo/App.kt
+++ ThreeDemo/composeApp/src/commonMain/kotlin/com/dcengineer/threedemo/App.kt
@@ -1,14 +1,27 @@
 package com.dcengineer.threedemo
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.multiplatform.webview.web.WebView
+import com.multiplatform.webview.web.rememberWebViewStateWithHTMLData
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    val html = """
+    <html>
+        <body>
+            <h1>Three.js will go here!</h1>
+            <p>Soon with JavaScript...</p>
+        </body>
+    </html>
+    """.trimIndent()
+    val webViewState = rememberWebViewStateWithHTMLData(
+        data = html
+    )
+
     MaterialTheme {
-        Text("Three.js will go here!")
+        WebView(webViewState)
     }
 }
