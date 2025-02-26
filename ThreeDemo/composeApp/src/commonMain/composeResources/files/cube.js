df263c2451c8c17555d1f90f21d7f9501dabb7fe
radcli14 <radcli14@gmail.com>
2025-02-26 10:56:04 +0100

    changed to importing a newer version of three.js inside cube.js, instead of importing in the HTML

--- ThreeDemo/composeApp/src/commonMain/composeResources/files/cube.js
+++ ThreeDemo/composeApp/src/commonMain/composeResources/files/cube.js
@@ -1,3 +1,5 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.173.0/build/three.module.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
